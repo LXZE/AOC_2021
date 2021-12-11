1ac2d12078d2e942e730312ee318b7eb7359f206
Hayashi Takui <hayashi.azi1mov@outlook.com>
2021-12-11 15:50:18 +0700

    Add day 11 solution :octocat:

---
+++ 11/2.py
@@ -0,0 +1,56 @@
+with open('input.txt') as f:
+	octopus = list(map(lambda line: list(map(int, line.strip())), f.readlines()))
+
+def print_grid(grid):
+	for line in grid:
+		print(''.join(map(str, line)))
+
+def update(grid):
+	flash_count = 0
+
+	# increase all
+	flashed = set()
+	to_updates = []
+	for row in range(len(grid)):
+		for col in range(len(grid[0])):
+			grid[row][col] += 1
+			if grid[row][col] >= 10:
+				flashed.add((row, col))
+				to_updates.extend(
+					list(filter(
+						lambda x: x[0] >= 0 and x[0] < len(grid) and x[1] >= 0 and x[1] < len(grid[0]),
+						[(row + delta_row, col + delta_col) for delta_row in range(-1, 2) for delta_col in range(-1, 2)]
+					))
+				)
+
+	# loop update
+	while len(to_updates) > 0:
+		row, col = to_updates.pop()
+		if (row, col) in flashed: continue
+		grid[row][col] += 1
+		if grid[row][col] >= 10:
+			flashed.add((row, col))
+			to_updates.extend(
+				list(filter(
+					lambda x: x[0] >= 0 and x[0] < len(grid) and x[1] >= 0 and x[1] < len(grid[0]),
+					[(row + delta_row, col + delta_col) for delta_row in range(-1, 2) for delta_col in range(-1, 2)]
+				))
+			)
+
+	# clear
+	for row in range(len(grid)):
+		for col in range(len(grid[0])):
+			if grid[row][col] >= 10:
+				grid[row][col] = 0
+				flash_count += 1
+
+def check_all_flash(grid):
+	return all(map(lambda line: sum(line) == 0, grid))	
+
+res = 0
+while True:
+	update(octopus)
+	res += 1
+	if check_all_flash(octopus):
+		break
+print(res)
