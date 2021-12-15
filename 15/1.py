046b033bd3e80b39f302b59d23abdecf3f0018e7
Hayashi Takui <hayashi.azi1mov@outlook.com>
2021-12-15 15:09:13 +0700

    Add day 15 solution :chart_with_downwards_trend:

---
+++ 15/1.py
@@ -0,0 +1,30 @@
+from collections import defaultdict as ddict
+import heapq
+
+with open('input.txt') as f:
+	maps = list(map(lambda line: list(map(int, line.strip())), f.readlines()))
+
+def solve():
+	h, w = len(maps), len(maps[0])
+	queue = [(0, (0,0))]
+	fp = ddict(lambda: float('inf'), { (0,0): 0 })
+	visited = set()
+	while queue:
+		dist, pos = heapq.heappop(queue)
+		if pos == (h-1, w-1): return dist
+		if pos in visited: continue
+		visited.add(pos)
+		row, col = pos
+
+		candidates = list(filter(
+			lambda p: 0 <= p[0] < h and 0 <= p[1] < w,
+			[(row-1, col), (row+1, col), (row, col-1), (row, col+1)]
+		))
+
+		for c in candidates:
+			new_dist = dist + maps[c[0]][c[1]]
+			if new_dist < fp[c]:
+				fp[c] = new_dist
+				heapq.heappush(queue, (new_dist, c))
+res = solve()
+print(res)
