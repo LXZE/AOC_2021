fdcbae6195f5042b9ada9c5a250fad5de8db3b81
Hayashi Takui <hayashi.azi1mov@outlook.com>
2021-12-17 18:37:57 +0700

    Add day 17 solution :fishing_pole_and_fish:

---
+++ 17/1.py
@@ -0,0 +1,18 @@
+with open('input.txt') as f:
+	line = f.readline().strip()
+	tmp = line.split('x=')
+	tmp = tmp[1].split(', y=')
+	x_range = list(map(int, tmp[0].split('..')))
+	y_range = list(map(int, tmp[1].split('..')))
+
+def feasible(pos_x, pos_y, vel_x, vel_y):
+	if x_range[0] <= pos_x <= x_range[1] and y_range[0] <= pos_y <= y_range[1]: return True
+	if pos_y < y_range[0] or pos_x > x_range[1]: return False
+	return feasible(pos_x + vel_x, pos_y + vel_y, 0 if vel_x == 0 else vel_x - 1, vel_y - 1)
+
+max_y = 0
+for velx in range(1, x_range[1] + 1):
+	for vely in range(100): # blindly guess, should not go beyond 100
+		if feasible(0,0, velx, vely):
+			max_y = max(max_y, vely)
+print((max_y*(max_y+1))//2)
